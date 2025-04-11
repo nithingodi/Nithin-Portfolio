30e8941e21e7dd68db7ac4b60c7cd0eb6f0a6693
nithingodi <154440857+nithingodi@users.noreply.github.com>
2025-04-10 20:46:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/nithin-portfolio-deploy/",  // 👈 your GitHub repo name here
+  base: "/nithin-godi-portfolio/",  
   plugins: [react()],
 })
